9dc1a8bf55e5536da722be028154ab9d73b75ce6
Nathan Naze <nanaze@gmail.com>
2016-10-11 21:40:10 -0700

    render out the items for spur (to start)

--- make_index.py
+++ make_index.py
@@ -15,10 +15,39 @@ def _YieldItems(csvfile):
   for row in csvreader:
     yield Item(*row)
 
-def main():
-  with open('spur.csv') as csvfile:
-    print list(_YieldItems(csvfile))
-    
+def _WriteTable(items, out):
+  out.write('<table>\n')
 
+  out.write('<tr>\n')  
+  for name, _, _ in items:
+    out.write('<th>%s</th>\n' % name)
+  out.write('</tr>\n')
+
+  out.write('<tr>')  
+  for _, val, url in items:
+    out.write('<td>\n')
+
+    out.write('<a href="%s">%s</a>\n' % (url, val))
+    out.write('</td>\n')
+
+  out.write('</tr>\n')  
+
+  out.write('</table>')
+
+def main():
+  out = sys.stdout
+
+  
+  spur_items = list(_YieldItems(open('spur.csv')))
+    
+  out.write('<!doctype html>\n')
+  out.write('<html>\n')
+  out.write('<body>\n')
+  out.write('<h1>SPUR</h1>')
+  _WriteTable(spur_items, out)
+  
+  out.write('</body>\n')
+  out.write('</html>\n')
+    
 if __name__ == '__main__':
   main()
